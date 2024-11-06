86fef0ca367d43ee6a2f13cd962e0f1f4f2b662d
RobinCreed <118176374+RobinCreed@users.noreply.github.com>
2024-11-06 13:18:57 +0100

    Connexion avec la base de données + Ajout des schémas User et Role

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,49 @@ import express from "express";
 import dotenv from "dotenv";
 
 import testRoute from "@routes/test";
+import User from "./models/User";
+import Role from "./models/Role";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// Connexion à MongoDB
+const db = process.env.DATABASE_URI as string || "";
+
+mongoose.connect(db)
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
+
 // Routes
 app.use('/', testRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
+const PORT = process.env.PORT as string | "8080";
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
+
+
+const user = new User({
+    firstName: "Jeremy",
+    lastName: "Dupont",
+    email: "jeremy.dupont@gmail.com",
+    activationDate: new Date(),
+    password: "azerty"
+});
+
+user.save();
+
+
+/*const admin = new Role({
+    name: "admin"
+});
+
+admin.save();*/
---
+++ src/models/User.ts
@@ -0,0 +1,43 @@
+import { ObjectId, Schema, model } from "mongoose";
+
+interface IUser {
+    firstName: String,
+    lastName: String,
+    password?: String,
+    email: String,
+    roles: ObjectId[],
+    birthdate: Date,
+    profilePicture: String,
+    activationDate?: Date,
+    activationToken?: String,
+    lastLogin?: Date,
+    refreshToken?: String
+}
+
+
+type Validator = (v: any) => Boolean; 
+
+const isEmailValid: Validator = (v: string) => {
+    return /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim.test(v);
+}
+
+
+
+const userSchema = new Schema<IUser>({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: [function() { return this.activationDate != undefined }, "An activated account must have a password."]},
+    roles: { type: [Schema.Types.ObjectId], ref: "Role", default: [] },
+    email: {
+        type: String,
+        validate: [isEmailValid, "{VALUE} is not a valid email."],
+        required: true
+    },
+    profilePicture: { type: String, required: false },
+    activationDate: { type: Date },
+    activationToken: { type: String },
+    lastLogin: { type: Date },
+    refreshToken: { type: String }
+}, { minimize: false });
+
+export default model("users", userSchema);
