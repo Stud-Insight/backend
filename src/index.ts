0ace2f7d26fe7eaa915a5c7fb205eedd2aa89253
ZeyXs <basile.gauttron@gmail.com>
2024-11-08 00:42:18 +0100

    Correction de l'erreur dans le tsconfig et correction du port undefined

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ import testRoute from "@routes/test";
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8080;
 app.use(express.json());
 
 // Routes
 app.use('/', testRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
