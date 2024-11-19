178cb205678af7fc65378f06648b95ea7d90f3b1
ZeyXs <basile.gauttron@gmail.com>
2024-11-19 11:36:13 +0100

    🧹 Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import config from 'config';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
-app.use(express.json());
+const PORT = process.env.PORT || 8080;
+const DATABASE_URI = process.env.DATABASE_URI || "";
 
-import cors from 'cors';
+app.use(express.json());
 app.use(cors());
 
 // Connexion à MongoDB
-const db = process.env.DATABASE_URI as string || "";
-
-mongoose.connect(db)
+mongoose.connect(DATABASE_URI)
     .then(() => {
         console.log('MongoDB connected successfully');
     })
@@ -27,28 +25,6 @@ mongoose.connect(db)
 app.use('/auth', require("@routes/auth"))
 app.use('/attachments', require("@routes/attachments"))
 
-const PORT = config.get('server.port') || "8080";
-
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
-
-/*
-const user = new User({
-    firstName: "Jeremy",
-    lastName: "Dupont",
-    email: "jeremy.dupont@gmail.com",
-    activationDate: new Date(),
-    password: "azerty"
-});
-
-user.save();
-*/
-
-/*const admin = new Role({
-    name: "admin"
-});
-
-admin.save();*/
-
-export default { db }
