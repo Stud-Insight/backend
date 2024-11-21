756c58c07104d91452a9492dca377f7ecf35c428
Anouk Oms <anouk@MacBook-Air-de-Anouk.local>
2024-11-21 15:02:01 +0100

    commencer la gestion des templates

--- src/controllers/mailerController.ts
+++ src/controllers/mailerController.ts
@@ -2,10 +2,9 @@ import { Request, Response } from "express";
 import nodemailer from "nodemailer";
 import utils from "./utils";
 
-
 //const envoyerMail = async (req:Request, res:Response) => {
 const trySendMail = async (req:Request, res:Response) => {
-  utils.sendMail("anouk.oms@caramail.fr,anouk.comb@gmail.com","ÇA MAAAAAAAARCHE !!","In sleep he sang to me,\n in dreams he came")
+  utils.sendMail("anouk.oms@caramail.fr","Mail par default!","default")
   .then(()=>{res.send("mail envoyé !")})
   .catch((error)=>{res.send({message:"Error !!", error:error})})
 }
--- src/controllers/utils.ts
+++ src/controllers/utils.ts
@@ -19,21 +19,21 @@ const transporter = nodemailer.createTransport({
 });
 
 
-const sendMail = async (receiversList:string, subject:string, text:string) => {
-  const htmlTemplate = await readFileAsync('./src/controllers/mailTemplate.html', 'utf-8');
+const sendMail = async (receivers:string, subject:string, template:string) => {
+  const htmlTemplate = await readFileAsync(`./src/controllers/mailTemplates/${template}.html`, 'utf-8');
   const content =  {
     from: "Stud'Insight <"+process.env.MAIL_CONTACT+">", // sender address
-    to: receiversList, // list of receivers
+    to: receivers, // list of receivers
     subject: subject, // Subject line
     html:htmlTemplate, // plain text body
   }
   transporter
     .sendMail(content)
     .then(()=>{
-      console.log({ message: "mail envoyé!", contente: content });
+      console.log({ message: "mail envoyé!"});
     })
     .catch((error)=>{
-      console.log({ message: "échec de l'envoie :(", error, content: content });
+      console.log({ message: "échec de l'envoie :(", error});
     });
 }
 
