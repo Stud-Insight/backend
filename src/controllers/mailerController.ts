680d7ca8edf19c1d28b9939693b8c4950715452f
Anouk Oms <anouk@MacBook-Air-de-Anouk.local>
2024-11-22 09:51:24 +0100

    pièces jointes et début de templates

--- src/controllers/mailerController.ts
+++ src/controllers/mailerController.ts
@@ -4,7 +4,8 @@ import utils from "./utils";
 
 //const envoyerMail = async (req:Request, res:Response) => {
 const trySendMail = async (req:Request, res:Response) => {
-  utils.sendMail("anouk.oms@caramail.fr","Mail par default!","default")
+  utils.sendMail("kilod54605@exoular.com","Mail par default!","new") //  ./src/controllers/test.txt
+  //utils.sendMailFile("lucile.bascoul15@gmail.com","Première connexion","default",[{path:"./src/controllers/test.txt"}]) //  ./src/controllers/test.txt
   .then(()=>{res.send("mail envoyé !")})
   .catch((error)=>{res.send({message:"Error !!", error:error})})
 }
--- src/controllers/utils.ts
+++ src/controllers/utils.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Attachment } from "nodemailer/lib/mailer";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import fs from 'fs';
@@ -10,7 +11,7 @@ const readFileAsync = promisify(fs.readFile);
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587, //sinon 587
+  port: 587,
   secure: false, // true for port 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
@@ -37,4 +38,24 @@ const sendMail = async (receivers:string, subject:string, template:string) => {
     });
 }
 
-export default { sendMail };
+//https://www.nodemailer.com/message/attachments/
+const sendMailFile = async (receivers:string, subject:string, template:string, attachmentsList:Attachment[]) => {
+  const htmlTemplate = await readFileAsync(`./src/controllers/mailTemplates/${template}.html`, 'utf-8');
+  const content =  {
+    from: "Stud'Insight <"+process.env.MAIL_CONTACT+">", // sender address
+    to: receivers, // list of receivers
+    subject: subject, // Subject line
+    html:htmlTemplate, // plain text body
+    attachments: attachmentsList, //Attachments list
+  }
+  transporter
+    .sendMail(content)
+    .then(()=>{
+      console.log({ message: "mail envoyé!"});
+    })
+    .catch((error)=>{
+      console.log({ message: "échec de l'envoie :(", error});
+    });
+}
+
+export default { sendMail, sendMailFile };
