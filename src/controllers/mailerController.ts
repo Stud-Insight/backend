0ef7fdcea5d0f07848d35b372ab8f46c98891486
Anouk Oms <anouk@MacBook-Air-de-Anouk.local>
2024-11-25 09:08:57 +0100

    premier template

--- src/controllers/mailerController.ts
+++ src/controllers/mailerController.ts
@@ -4,7 +4,7 @@ import utils from "./utils";
 
 //const envoyerMail = async (req:Request, res:Response) => {
 const trySendMail = async (req:Request, res:Response) => {
-  utils.sendMail("kilod54605@exoular.com","Mail par default!","new") //  ./src/controllers/test.txt
+  utils.sendMail("arthur.defays@gmail.com","Mail par default!","firstTry") //  ./src/controllers/test.txt
   //utils.sendMailFile("lucile.bascoul15@gmail.com","Première connexion","default",[{path:"./src/controllers/test.txt"}]) //  ./src/controllers/test.txt
   .then(()=>{res.send("mail envoyé !")})
   .catch((error)=>{res.send({message:"Error !!", error:error})})
