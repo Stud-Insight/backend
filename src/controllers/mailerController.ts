fcb716430e1e674030b88a90500c1391a4471448
Anouk Oms <anouk@MacBook-Air-de-Anouk.local>
2024-11-27 13:35:10 +0100

    organisation template

--- src/controllers/mailerController.ts
+++ src/controllers/mailerController.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from "express";
+import nodemailer from "nodemailer";
 import utils from "../utils/mailer";
 
 //const envoyerMail = async (req:Request, res:Response) => {
-const trySendMail = async (req:Request, res:Response) => {
-  utils.sendMail("robin.deroch@etu.umontpellier.fr","Première connexion","firstConnect") //  ./src/controllers/test.txt
-  //utils.sendMailFile("lucile.bascoul15@gmail.com","Première connexion","default",[{path:"./src/controllers/test.txt"}]) //  ./src/controllers/test.txt
-  .then(()=>{res.send("mail envoyé !")})
-  .catch((error)=>{res.send({message:"Error !!", error:error})})
+const trySendMail = async (email:string, subject:string) => {
+  console.log(email,subject,)
+  utils.sendMail(email,subject,"firstTry")
 }
 
 export default { trySendMail };
