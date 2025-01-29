d2ac95064d994dc1e5b1d1f57594086369718438
RobinCreed <118176374+RobinCreed@users.noreply.github.com>
2025-01-29 17:01:34 +0100

    Utilisation des interfaces IChat_Mongoose et IMessage_Mongoose dans leur schema respectif

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -1,23 +1,23 @@
 import config from "@/config/config";
 import ChatType from "@/enums/ChatType";
-import IChat from "@/interfaces/IChat";
-import IMessage from "@/interfaces/IMessage";
+import { IChat_Mongoose } from "@/interfaces/IChat";
+import { IMessage_Mongoose } from "@/interfaces/IMessage";
 import { model, Schema } from "mongoose"
 
-const messageSchema = new Schema<IMessage>({
+const messageSchema = new Schema<IMessage_Mongoose>({
     sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
     sentAt: { type: Date, required: true },
     content: { type: String, required: true },
     attachments: { type: [Schema.Types.ObjectId], ref: config.database.filesBucketName + ".files" },
 });
 
-const chatSchema = new Schema<IChat>({
+const chatSchema = new Schema<IChat_Mongoose>({
     members: { type: [Schema.Types.ObjectId], ref: "User", default: [], required: true },
     type: { type: String, enum: Object.values(ChatType), required: true },
     groupName: { type: String, required: function() { return this.type == ChatType.GROUP; } },
     groupCreator: { type: Schema.Types.ObjectId, required: function() { return this.type == ChatType.GROUP; } },
     groupCreatedAt: { type: Date, required: function() { return this.type == ChatType.GROUP; } },
-    messages: { type: [messageSchema], required: true }
+    messages: { type: [messageSchema], default: [], required: true }
 });
 
 export default model("chats", chatSchema);
